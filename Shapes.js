f79f1cf0574ba75a96017a565ce45707e65584ba
PedroDepa <pedro.depauli22@gmail.com>
2022-08-31 11:57:10 -0300

    Actualizacion de figuritas

--- Shapes.js
+++ Shapes.js
@@ -10,6 +10,9 @@ class Shape {
     toString() {
         return Object.getPrototypeOf(this).constructor.name;
     }
+    semiperimetro() {
+        return 0;
+    }
 }
   
 class Circle extends Shape {
@@ -57,7 +60,55 @@ class Triangle extends Shape {
     }
 }
 
-export {Circle, Rectangle, Square, Triangle};
+
+class Pentagon extends Shape {
+    constructor(a, b) {
+        super();
+        this.apothem = a;
+        this.base = b;
+    }
+    
+    perimeter() {
+        return 5 * this.base
+    }
+
+    area() {
+        return this.perimeter() * this.apothem / 2
+    }
+
+}
+
+class Rhomboid extends Shape {
+    constructor(w, h) {
+        super();
+        this.width = w;
+        this.height = h;
+    }
+    area() {
+        return this.width * this.height;
+    }
+
+    perimeter() {
+        return 2 * (this.width + this.height);
+    }
+}
+
+
+class TriangleV2 extends Shape{
+    constructor(a, b, c) {
+        super();
+        this.lado1 = a;
+        this.lado2 = b;
+        this.lado3 = c;
+    }
+    semiperimetro() {
+        return (this.lado1 + this.lado2 +this.lado3)/2;
+    }
+    area() {
+        return  Math.sqrt(this.semiperimetro()*(this.semiperimetro() - this.lado1)*(this.semiperimetro()-this.lado2)*(this.semiperimetro()- this.lado3));
+    }
+}
+export {Circle, Rectangle, Square, Triangle, Pentagon, Rhomboid, TriangleV2}
 
 
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {Circle, Rectangle, Square, Triangle} from './Shapes.js';
+import {Circle, Rectangle, Square, Triangle, Pentagon, Rhomboid, TriangleV2} from './Shapes.js';
 
 const triangle = new Triangle(10, 5);
 const triangle2 = new Triangle(9, 19);
@@ -13,3 +13,14 @@ console.log(`Area: ${rectangle.area()} - Perimeter: ${rectangle.perimeter()}`);
 
 const circle = new Circle(10);
 console.log(circle.perimeter());
+
+const pentagon = new Pentagon(5, 10)
+console.log(`Area: ${pentagon.area()} - Perimeter: ${pentagon.perimeter()}`);
+
+const rhomboid = new Rhomboid(10, 5)
+console.log(`Area: ${rhomboid.area()} - Perimeter: ${rhomboid.perimeter()}`)
+
+const trianglev2 = new TriangleV2(5, 7, 9);
+console.log(`Area: ${trianglev2.area()} - Perimeter: ${trianglev2.perimeter()} - Semiperimetro ${trianglev2.semiperimetro()}`);
+
+
